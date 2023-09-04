a22989a51070f4756fd604cb1af78ff25977f5c6
Sourabh Vartak <vartak.sourabh@gmail.com>
2023-09-04 22:39:37 +0400

    fixing the bug that extra_context does not work

--- home/views.py
+++ home/views.py
@@ -5,8 +5,11 @@ from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 class HomeView(TemplateView):
-    template_name = 'home/welcome.html' 
-    extra_context = {'today': datetime.today()}
+    template_name = 'home/welcome.html'
+    def get_context_data(self, **kwargs): 
+        context = super().get_context_data(**kwargs) 
+        context['today'] = datetime.today() 
+        return context
 
 class AuthorizedView(LoginRequiredMixin, TemplateView):
     template_name = 'home/authorized.html'
